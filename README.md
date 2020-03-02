273009bdee3778ff0a9932a794d653009c248374
jenspetur94 <jenspeturtrondarson@gmail.com>
2020-03-02 15:23:35 +0100

    first action test

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# add-message-to-body-pr
-Git action for adding a standard message to the body of all pr's
+# Add a message to the body of a PR via GitHub Action
+
+A GitHub action for automatically adding a message to the end of a PR's body
+
+## Usage
+
+- Requires the `GITHUB_TOKEN` secret.
+- Requires the message int the `msg` parameter.
+
+### Sample workflow
+
+```yml
+name: add-message-to-pr
+
+on: pull_request
+
+jobs:
+  add_msg:
+    runs-on: ubuntu-latest
+    steps:
+    - uses:jenspetur94/add-message-to-body-pr@v1
+      with:
+        repo-token: "${{ secrets.GITHUB_TOKEN }}"
+        msg: 'message to be added'
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run(){
+	try {
+		const inputs = {
+			token: core.getInput('repo-token', {required: true}),
+			msg: core.getInput('msg', {required: true})
+		};
+
+		const request = {
+			owner: github.context.repo.owner,
+			repo: github.context.repo.repo,
+			pull_number: github.context.payload.pull_request.number
+		}
+		const currentBody = github.context.payload.pull_request.body;
+		const shouldUpdateBody = !currentBody.includes(inputs.msg);
+		if(shouldUpdateBody){
+			request.body = currentBody.concat('\n\n', inputs.msg);
+			core.debug(`Added ${inputs.msg} to end of body.`)
+		}else{
+			core.warning('Body contains message. Body was not updated');
+			return;
+		}
+
+		const client = new github.GitHub(inputs.token);
+		const response = await client.pulls.update(request);
+		
+		core.info(`response: ${response.status}`);
+		if(response.status !== 200){
+			core.error('Failed to update pull request');
+		}
+	}
+	catch(error){
+		core.error(error);
+		core.setFailed(error.message);
+	}
+}
+
+run()
