5665aaf6083e6470a6f4f3e9de66b9d6acf4e8ab
jenspetur94 <jenspeturtrondarson@gmail.com>
2020-03-04 11:17:54 +0100

    throwing errors instead of try catching

--- src/compareRegex.js
+++ src/compareRegex.js
@@ -2,23 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 exports.run = async function (token, regex){
-	try{
-		const request = {
-			owner: github.context.repo.owner,
-			repo: github.context.repo.repo,
-			pull_number: github.context.payload.pull_request.number
-		}
-		const currentTitle = github.context.payload.pull_request.title;
-		const titleIsValid = regex.test(currentTitle);
-		if(!titleIsValid){
-			core.error(`Title did not match patter: ${regex.toString()}`);
-			core.setFailed(`Title did not match patter: ${regex.toString()}`);
-			return;
-		}
-		core.endGroup();
-		return;
-	}catch(error){
-		core.error(error);
-		core.setFailed(error.message);
+	const request = {
+		owner: github.context.repo.owner,
+		repo: github.context.repo.repo,
+		pull_number: github.context.payload.pull_request.number
 	}
+	const currentTitle = github.context.payload.pull_request.title;
+	const titleIsValid = regex.test(currentTitle);
+	if(!titleIsValid){
+		core.error(`Title did not match patter: ${regex.toString()}`);
+		throw new Error(`Title did not match patter: ${regex.toString()}`);
+	}
+	core.debug('Title matched the pattern!');
+	core.endGroup();
+	return;
 }
--- src/messageToDescription.js
+++ src/messageToDescription.js
@@ -2,36 +2,33 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 exports.run = async function (token, msg){
-	try{
-		const request = {
-			owner: github.context.repo.owner,
-			repo: github.context.repo.repo,
-			pull_number: github.context.payload.pull_request.number
-		}
-		const currentBody = github.context.payload.pull_request.body;
-		const shouldUpdateBody = !currentBody.includes(msg);
-		
-		if(shouldUpdateBody){
-			request.body = currentBody.concat('\n\n', msg);
-			core.debug(`Added ${msg} to end of body.`)
-		}else{
-			core.warning('Body contains message. Body was not updated');
-			return;
-		}
+	const request = {
+		owner: github.context.repo.owner,
+		repo: github.context.repo.repo,
+		pull_number: github.context.payload.pull_request.number
+	}
+	const currentBody = github.context.payload.pull_request.body;
+	const shouldUpdateBody = !currentBody.includes(msg);
 	
-		const client = new github.GitHub(token);
-		const response = await client.pulls.update(request);
-		
-		core.info(`response: ${response.status}`);
-		if(response.status !== 200){
-			core.error('Failed to update pull request');
-		}
-		core.endGroup();
-		return;
-	}catch(error){
-		core.error(error);
-		core.setFailed(error.message);
+	if(shouldUpdateBody){
+		request.body = currentBody.concat('\n\n', msg);
+		core.debug(`Added ${msg} to end of body.`)
+	}else{
+		core.warning('Body contains message. Body was not updated');
 		return;
 	}
+
+	const client = new github.GitHub(token);
+	const response = await client.pulls.update(request);
+	
+	core.info(`response: ${response.status}`);
+	if(response.status !== 200){
+		core.error('Failed to update pull request');
+		throw new Error('Failed to update pull request')
+	}
+	core.debug('description was updated successfully!');
+	core.endGroup();
+	return;
+	
 }
 
